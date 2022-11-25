532bf051a08437d52fb782e913abc9aacee924b2
Michail Koryukin <kom1969@list.ru>
2022-11-25 20:13:29 +0500

    Завершение проекта2~

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',  # Добавленная запись
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -99,15 +100,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-INSTALLED_APPS = [
-    'posts.apps.PostsConfig',  # Добавленная запись
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
